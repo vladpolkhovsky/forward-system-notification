0f24ea330aae10a1493a07afe863fbd7b61a14eb
vladpolkhovsky <vlad.polkhovsky@gmail.com>
2026-04-22 23:04:27 +0300

    22.04.2026

--- backend/src/main/java/by/fwsys/bot/bot_app/listeners/ApplicationReadyEventListener.java
+++ backend/src/main/java/by/fwsys/bot/bot_app/listeners/ApplicationReadyEventListener.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
@@ -34,5 +35,17 @@ public class ApplicationReadyEventListener {
     private void stopAll(TelegramBotsLongPollingApplication tgApp, VkNotificationBot vkApp) {
         tgApp.stop();
         vkApp.stop();
+    }
+
+    @SneakyThrows
+    @Scheduled(cron = "0 0 * * * *")
+    public void stopVk() {
+        vkNotificationBot.stop();
+    }
+
+    @SneakyThrows
+    @Scheduled(cron = "0 3 * * * *")
+    public void startVk() {
+        vkNotificationBot.init();
     }
 }
